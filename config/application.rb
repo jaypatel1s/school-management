2af07266d0c4a43fbbf09e8c903ad89b52c0306e
jay-gami <jay.gami@softices.com>
2024-11-20 12:02:02 +0530

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SchoolManagementSystem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.eager_load = nil
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
