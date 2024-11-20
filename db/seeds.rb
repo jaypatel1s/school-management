867c347e7c83158fb187e10c68b9ce0cda6d9b1e
jay-gami <jay.gami@softices.com>
2024-11-20 13:16:31 +0530

    fxi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-admin_user = AdminUser.find_or_initialize_by(email: 'admin@example.com')
-admin_user.password = 'password'  # This will set the password_digest field automatically
-admin_user.password_confirmation = 'password'
-admin_user.save!
-college = College.find_or_initialize_by(name: 'First')
-college.save!  # Save the College if it's new
-user = User.find_or_initialize_by(college_id: college.id, email: 'jay@softices.in', password: '123456', password_confirmation: '123456', role: 'principal', name: 'Jay', confirmed_at: Time.now)
-user.save!  # Save the User if it's new or updated
+unless AdminUser.any?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
+
+unless College.any?
+  college = College.create!(name: 'First')
+end
+
+unless User.any?
+ User.create!(college_id: College.first.id, email: 'jay@softices.in', password: '123456', password_confirmation: '123456', role: 'principal', name: 'Jay', confirmed_at: Time.now)
+end
