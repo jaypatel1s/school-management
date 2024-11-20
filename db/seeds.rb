8368d863e86d1d59e940a97af5fd857adabe09f7
jay-gami <jay.gami@softices.com>
2024-11-20 13:02:29 +0530

    fxi

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 admin_user = AdminUser.find_or_initialize_by(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-admin.save!
+admin_user.save!
 college = College.find_or_initialize_by(name: 'First')
 college.save!  # Save the College if it's new
 user = User.find_or_initialize_by(college_id: college.id, email: 'jay@softices.in', password: '123456', password_confirmation: '123456', role: 'principal', name: 'Jay', confirmed_at: Time.now)
