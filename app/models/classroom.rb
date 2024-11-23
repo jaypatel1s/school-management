f17a1ef716cc17a12ea4a6edde45aee48263286a
jaypatel1s <90921666+jaypatel1s@users.noreply.github.com>
2024-11-23 19:31:19 +0530

    Jay 1 (#6)
    * fix login button
    
    * fix signout button
    
    * added classroom session

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,7 +5,6 @@ class Classroom < ApplicationRecord
   include Sluggable
 
   belongs_to :college
-
   has_many :teacher_classrooms, dependent: :destroy
   has_many :teacher_subjects, through: :teacher_classrooms
 end
--- app/models/college.rb
+++ app/models/college.rb
@@ -2,8 +2,7 @@
 
 class College < ApplicationRecord
   include Sluggable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :users, dependent: :destroy
   has_many :subjects, dependent: :destroy
   has_many :classrooms, dependent: :destroy
