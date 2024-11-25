827b525b7613ff3c2baf02f171c5de493f35eb50
jay-gami <jay.gami@softices.com>
2024-11-25 12:00:13 +0530

    add domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,16 +93,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.asset_host = ENV['HOST']
-
   config.action_mailer.default_url_options = { :host =>  ENV['HOST'], protocol: 'https' }
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USER'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: :plain,  # or :login or :cram_md5 depending on your provider
+    authentication: :plain,
     enable_starttls_auto: true
   }
 end
