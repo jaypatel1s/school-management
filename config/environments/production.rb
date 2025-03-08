55d686ac6e602185512e50c54770bda6868ada55
jay <gamijay1@gmail.com>
2025-03-08 15:26:57 +0530

    Fix smtp setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.asset_host = Rails.application.credentials.dig(:smtp, :smtp_host)
-  config.action_mailer.default_url_options = { host:  Rails.application.credentials.dig(:smtp, :smtp_host), protocol: 'https' }
+  config.action_mailer.default_url_options = { host:  Rails.application.credentials.dig(:smtp, :smtp_host), protocol: 'http' }
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
