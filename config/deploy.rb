3f37ade8e68e1c96a6555bf6aac2c2946faae502
jay <gamijay1@gmail.com>
2025-03-08 10:56:48 +0530

    Fix deploymnet

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,5 @@ set :deploy_to, '/home/ubuntu/school'
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 set :keep_releases, 5
-set :rvm_ruby_version, '3.2.2'
+set :rvm_ruby_version, '3.2.0'
 
