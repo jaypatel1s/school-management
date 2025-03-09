07bf80f016a6254ca0ce4bdde5cf0af393a918d7
jay <gamijay1@gmail.com>
2025-03-09 11:47:41 +0530

    prod fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.hosts << Rails.application.credentials[:app_host]
+  config.hosts << Rails.application.credentials[:app_host_domain]
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
