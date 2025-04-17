4c566cca950f4a389a31645fd7d9ab96b90d9300
jay <gamijay1@gmail.com>
2025-04-17 19:11:58 +0530

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config/deploy.rb
 set :application, 'school'
 set :repo_url, 'https://github.com/jaypatel1s/school-management.git'
-set :branch, 'staging'
+set :branch, 'main'
 set :deploy_to, '/home/ubuntu/school'
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
