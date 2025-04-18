b28b087365cc28b7d3d05059db3091788bb127c7
jay <gamijay1@gmail.com>
2025-04-18 23:45:03 +0530

    fix mail

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -2,7 +2,7 @@ class TeacherMailer < ApplicationMailer
 
   def profile_approved(user)
     @teacher = user # Pass the user object
-    @teacher_subjects = @teacher.teacher_subjects
+    # @teacher_subjects = @teacher.teacher_subjects
 
     mail(to: user.email, subject: 'Your Profile Has Been Approved')
   end
