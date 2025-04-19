aa81c0c7e694fb57db0fa88f3755c2eebaf5f621
jay <gamijay1@gmail.com>
2025-04-19 13:20:39 +0530

    fix admin confirm controller

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,8 +1,6 @@
 ActiveAdmin.register User do
-  # Permitting parameters for user, excluding email and password on edit
   permit_params :college_id, :email, :profile_setup, :password_confirmation, :password, :name, :role, :contact_no, :description, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at
 
-  # Use the form block to conditionally exclude email and password on edit
   form do |f|
     f.semantic_errors
     f.inputs 'User' do
@@ -19,5 +17,13 @@ ActiveAdmin.register User do
       f.input :profile_setup, as: :boolean, label: "Profile Setup"
     end
     f.actions
+  end
+
+  controller do
+    before_save do |user|
+      if params['user']['confirmed_at'] == '1'
+        user.confirmed_at = Time.now
+      end
+    end
   end
 end
