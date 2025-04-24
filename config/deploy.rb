6b92fd3b14a9dc706b017ed4501c9b09e386eeda
jay <gamijay1@gmail.com>
2025-04-24 18:53:47 +0530

    fix54.167.122.206

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,9 @@ set :application, 'school'
 set :repo_url, 'https://github.com/jaypatel1s/school-management.git'
 set :branch, 'main'
 set :deploy_to, '/home/ubuntu/school'
+set :linked_files, %w{config/database.yml config/master.key config/credentials.yml.enc}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 set :keep_releases, 5
 set :rvm_ruby_version, '3.2.2'
 set :passenger_restart_with_touch, true
 
-append :linked_dirs, 'log', 'tmp', 'storage', '.bundle', 'config/credentials'
