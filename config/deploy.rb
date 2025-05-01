8415f117e49e6e45a60c767e07ad19120d8ffe0a
jay <gamijay1@gmail.com>
2025-05-01 11:25:05 +0530

    fix private

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config/deploy.rb
 set :application, 'school'
-set :repo_url, 'https://github.com/jaypatel1s/school-management.git'
+set :repo_url, 'git@github.com:jaypatel1s/school-management.git'  # Use SSH URL
 set :branch, 'main'
 set :deploy_to, '/home/ubuntu/school'
 set :linked_files, %w{config/database.yml config/master.key config/credentials.yml.enc}
