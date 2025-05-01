24ed92420100afefff16c2784c0606009e247eab
jay <gamijay1@gmail.com>
2025-05-01 11:17:06 +0530

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-# config/deploy/production.rb
 server '54.167.122.206', user: 'ubuntu', roles: %w{app db web}
+
 set :ssh_options, {
-  keys: %w(/home/jay/Downloads/college.pem),
+  keys: %w(/home/ubuntu/.ssh/id_rsa),
   forward_agent: false,
   auth_methods: %w(publickey)
 }
