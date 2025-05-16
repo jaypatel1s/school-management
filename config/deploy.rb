c9d3c24730678b13001a19b63dd5f12ccdd1187b
jay <gamijay1@gmail.com>
2025-05-16 20:54:04 +0530

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 set :application, 'school'
 set :repo_url, 'git@github.com:jaypatel1s/school-management.git'  # Use SSH URL
 set :branch, 'main'
-set :deploy_to, '/home/ubuntu/school'
+set :deploy_to, '/home/jay/public/college'
 append :linked_dirs, 'log', 'tmp', 'storage', '.bundle'
 append :linked_files, 'config/database.yml', 'config/master.key', 'config/credentials.yml.enc'
 
@@ -11,5 +11,4 @@ append :linked_files, 'config/database.yml', 'config/master.key', 'config/creden
 # set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 set :keep_releases, 5
 set :rvm_ruby_version, '3.2.2'
-set :passenger_restart_with_touch, true
 
