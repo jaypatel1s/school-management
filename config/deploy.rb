43e01cef180b5d0e311507e2eee9edf9cbaa8ec5
jay <gamijay1@gmail.com>
2025-05-23 21:07:49 +0530

    deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ set :repo_url, 'git@github.com:jaypatel1s/school-management.git'
 set :branch, 'main'
 set :deploy_to, '/home/jay/public/college'
 
-append :linked_dirs, %w(log tmp storage .bundle)
-append :linked_files, %w(config/database.yml config/master.key config/credentials.yml.enc)
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/uploads', 'storage', '.bundle'
+append :linked_files, 'config/database.yml', 'config/master.key', 'config/credentials.yml.enc'
 
 set :keep_releases, 5
 set :rvm_ruby_version, '3.2.0'
