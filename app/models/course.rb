c7174d7228052309a072c923b0cf8ebae9a0b971
jay <gamijay1@gmail.com>
2025-06-06 20:34:31 +0530

    fix reference

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,13 +7,13 @@ class Course < ApplicationRecord
   belongs_to :department
   has_many :student_courses
   has_many :students, through: :student_courses
+  has_one :teacher, dependent: :destroy
   has_many :course_enrollments
   has_many :fees, dependent: :destroy
   has_many :fee_types, through: :fees
   has_many :sessions, dependent: :destroy
-  has_many :students, through: :course_enrollments, source: :user
   has_many :assignments
 
-  validates :name, presence: true, uniqueness: {scope: :college_id}
+  validates :name, presence: true, uniqueness: { scope: :college_id }
   validates :credits, presence: true
 end
