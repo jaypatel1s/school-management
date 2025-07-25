b43a417958a4ab820884e646cca5901a15dbb009
jay-gami <jay.gami@softices.com>
2025-07-26 00:02:38 +0530

    Feature #000 - add regenerate token if invalid and generate new token and send email and if not expires then send mail

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -25,6 +25,7 @@ class Admission < ApplicationRecord
 
   validates :application_number, :temporary_token, presence: true, uniqueness: { scope: :college_id }
 
+  validate :unique_admission_by_email_and_course
   validate :expiry_date_validity, if: -> { user.nil? }
 
   scope :active, -> { where('expires_at > ? OR user_id IS NOT NULL', Time.current) }
@@ -43,12 +44,27 @@ class Admission < ApplicationRecord
   end
 
   def set_expiry
-    self.expires_at ||= 1.day.from_now if user.nil?
+    self.expires_at ||= 10.seconds.from_now if user.nil?
   end
 
   def expiry_date_validity
     return unless expires_at.present? && expires_at <= Time.current
 
     errors.add(:expires_at, 'must be in the future for guest applications')
+  end
+
+  def unique_admission_by_email_and_course
+    existing = Admission.where(
+      email: email, college_id: college_id, department_id: department_id, course_id: course_id
+    )
+
+    existing = existing.where.not(id: id) if persisted?
+
+    return unless existing.exists?
+
+    errors.add(:email, 'has already been.')
+    errors.add(:college_id, 'has already been.')
+    errors.add(:department_id, 'has already been')
+    errors.add(:course_id, 'has already been')
   end
 end
